bd9f18d2b3c0273a270d0f76873a6c2820425bb6
Pavel Romanenko <p.rom@me.com>
2021-02-22 08:39:00 +0100

    Add copying tweets ETL from MongoDB to Postgress

--- docker_project/etl_job/etl.py
+++ docker_project/etl_job/etl.py
@@ -2,10 +2,7 @@ import pymongo
 import time
 from sqlalchemy import create_engine
 
-time.sleep(10)  # seconds
-
-# Establishing a Postgres DB connection: 
-pg = create_engine('postgres://pavel:tweet123@host:5432/tweets_postgres_db', echo=True)
+time.sleep(10)  # Time (in seconds) to give Mongo a few seconds to start
 
 # Establish a connection to the MongoDB server
 client = pymongo.MongoClient("mongodb")
@@ -16,7 +13,21 @@ db = client.tweets
 # Select the collection of documents you want to use within the MongoDB database
 collection = db.tweet_data
 
-# Read a few entries from MongoDB and print them
-entries = collection.find(limit=5)
+# Establishing a Postgres DB connection: 
+pg = create_engine('postgres://pavel:tweet123@postgresdb:5432/tweets_postgres_db', echo=True)
+
+pg.execute('''
+    CREATE TABLE IF NOT EXISTS tweets (
+    text VARCHAR(500),
+    sentiment NUMERIC
+);
+''')
+
+# Read a few entries from MongoDB, print them, and add them to the Postgres databse
+entries = collection.find()
 for e in entries:
     print(e)
+    text = e['text']
+    score = 1.0  # placeholder value
+    query = "INSERT INTO tweets VALUES (%s, %s);"
+    pg.execute(query, (text, score))
